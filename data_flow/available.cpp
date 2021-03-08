96aa9ead40f61b4c82285ae80254c9d3a179d9be
Tanmaya Mishra <tanmayam@vt.edu>
2021-03-08 15:15:46 -0500

    Incorrect boundary condition for available fixed

--- data_flow/available.cpp
+++ data_flow/available.cpp
@@ -46,7 +46,7 @@ namespace {
       unsigned size_bitvec = exp_bvec_mapping.size();
 
       //initialize data flow framework
-      DFF dff(&F, false, INTERSECTION, size_bitvec, &transfer_function, true);
+      DFF dff(&F, false, INTERSECTION, size_bitvec, &transfer_function, false);
 
       // compute use and def sets here
       populate_egen_ekill(F);
